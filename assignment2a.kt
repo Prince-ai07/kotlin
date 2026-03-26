bdbdfe77299d8a66d169c8a1af34ee8164660c44
Prince-ai07 <jeremiahprince342@gmail.com>
2026-03-26 11:05:34 +0300

    added assignment 2

---
+++ assignment2a.kt
@@ -0,0 +1,12 @@
+// Using if or when statements, create a program to find if given year is leap or not 
+fun main() {
+    val year = 2024
+
+    when {
+        (year % 400 == 0) -> println("Year is a leap year")
+        (year % 100 == 0) -> println("Year is not a leap year")
+        (year % 4 == 0) -> println("Year is a leap year")
+        else -> println("Year is not a leap year")
+    }
+}
+
---
+++ assignment2b.kt
@@ -0,0 +1,11 @@
+// Using if statements check is a given number is odd or even
+fun main() {
+    val number = 4
+
+    if (number % 2 == 0) {
+        println("Even")
+    }
+    else {
+        println("Odd")
+    }
+}
